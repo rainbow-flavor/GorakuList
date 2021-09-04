14439a78655207ec00b95a211f6e36faa60bb51c
RBK <martinok1103@naver.com>
2021-09-05 01:56:29 +0900

    detail search name tag

--- js/searchBtn.js
+++ js/searchBtn.js
@@ -2,5 +2,74 @@ function searchCond() {
     city1 = $("#city1").val();
     city2 = $("#city2").val();
 
-    location.href = "/GorakuList/search.html?city1="+city1+"&city2="+city2;
+    if(city1 == null) {
+        alert("시/도를 선택하세요.");
+        return -1;
+    }
+    if(city2 == null) {
+        alert("시/군/구를 선택하세요.");
+        return -1;
+    }
+
+    var count = 0;
+    var numlist = '';
+    var gamelist = '';
+    $.each(game1, function(idx, item) {
+        if($("input:checkbox[id='gid"+item.id+"']").is(":checked") == true) {
+            count += 1;
+            numlist += item.id+',';
+            gamelist += item.name+',';
+        }
+    });
+    $.each(game2, function(idx, item) {
+        if($("input:checkbox[id='gid"+item.id+"']").is(":checked") == true) {
+            count += 1;
+            numlist += item.id+',';
+            gamelist += item.name+',';
+        }
+    });
+    $.each(game3, function(idx, item) {
+        if($("input:checkbox[id='gid"+item.id+"']").is(":checked") == true) {
+            count += 1;
+            numlist += item.id+',';
+            gamelist += item.name+',';
+        }
+    });
+    $.each(game4, function(idx, item) {
+        if($("input:checkbox[id='gid"+item.id+"']").is(":checked") == true) {
+            count += 1;
+            numlist += item.id+',';
+            gamelist += item.name+',';
+        }
+    });
+    $.each(game5, function(idx, item) {
+        if($("input:checkbox[id='gid"+item.id+"']").is(":checked") == true) {
+            count += 1;
+            numlist += item.id+',';
+            gamelist += item.name+',';
+        }
+    });
+    $.each(game6, function(idx, item) {
+        if($("input:checkbox[id='gid"+item.id+"']").is(":checked") == true) {
+            count += 1;
+            numlist += item.id+',';
+            gamelist += item.name+',';
+        }
+    });
+    $.each(game7, function(idx, item) {
+        if($("input:checkbox[id='gid"+item.id+"']").is(":checked") == true) {
+            count += 1;
+            numlist += item.id+',';
+            gamelist += item.name+',';
+        }
+    });
+    console.log(numlist)
+    console.log(gamelist)
+
+    if(count >= 6) {
+        alert("5개 이상 선택하실 수 없습니다!");
+        return -1;
+    }
+
+    location.href = "/GorakuList/search.html?city1="+city1+"&city2="+city2+"&game="+numlist.slice(0, -1);
 }
