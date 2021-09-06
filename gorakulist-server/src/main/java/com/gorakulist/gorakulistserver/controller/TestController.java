15db413c05720dc7d507112034fc44dac919d74f
irostub <irostub@gmail.com>
2021-09-07 08:14:27 +0900

    Update cors domain

--- gorakulist-server/src/main/java/com/gorakulist/gorakulistserver/controller/TestController.java
+++ gorakulist-server/src/main/java/com/gorakulist/gorakulistserver/controller/TestController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
+@CrossOrigin("*")
 @RestController
 public class TestController {
 
--- gorakulist-server/src/main/java/com/gorakulist/gorakulistserver/settings/SecuritySetting.java
+++ gorakulist-server/src/main/java/com/gorakulist/gorakulistserver/settings/SecuritySetting.java
@@ -12,8 +12,10 @@ public class SecuritySetting implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:8080",
                         "http://gorakulist.herokuapp.com",
+                        "http://www.gorakulist.kro.kr",
 
                         "https://localhost:8080",
-                        "https://gorakulist.herokuapp.com");
+                        "https://gorakulist.herokuapp.com",
+                        "https://www.gorakulist.kro.kr");
     }
 }
