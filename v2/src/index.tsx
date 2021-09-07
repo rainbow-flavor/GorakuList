8cdc7cf9bb4df8d695d149bde818fb57fa888014
SieR-VR <nwh63@naver.com>
2021-09-07 09:49:57 +0900

    Update V2

--- v2/src/index.tsx
+++ v2/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Nav from './components/navbar';
-import Footer from './components/footer';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './app';
+
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Nav />
-    <Footer />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
