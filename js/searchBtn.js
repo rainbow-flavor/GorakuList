1d18c2eb0a5c1b1b3fa300b938880374cff34d9a
SieR-VR <nwh63@naver.com>
2021-09-11 22:30:03 +0900

    Add searched shop cards to search window

--- js/searchBtn.js
+++ js/searchBtn.js
@@ -1,3 +1,10 @@
+const openedElementList = [
+    '<span class="badge bg-success" style="color:white">영업 중</span>',
+    '<span class="badge bg-danger" style="color:white">폐업</span>',
+    '<span class="badge bg-warning" style="color:white">영업 종료</span>',
+    '<span class="badge bg-secondary" style="color:white">정보 없음</span>'
+]
+
 function searchCond() {
     city1 = $("#city1").val();
     city2 = $("#city2").val();
@@ -16,7 +23,7 @@ function searchCond() {
     var numlist = new Array();
     var gamelist = '';
 
-    function checkIsChecked(idx, item) {
+    function checkIsChecked(item) {
         if ($("input:checkbox[id='gid" + item.id + "']").is(":checked") == true) {
             count += 1;
             numlist.push(item.id);
@@ -39,13 +46,8 @@ function searchCond() {
         machineTypes: []
     }
 
-    if (count == 0) {
-        search(data);
-    }
-    else {
-        data.machineTypes = numlist;
-        search(data);
-    }
+    data.machineTypes = numlist;
+    search(data);
 }
 
 function search(data) {
@@ -58,7 +60,39 @@ function search(data) {
         crossOrigin: true
     })
     .done(function (json) {
-        //TODO: json 데이터 받아서 동적으로 html 생성해야하는 부분
-        console.log(json)
+        if (json.length == 0) return;
+
+        console.log(json);
+
+        let shopsHtml = '';
+        $("#shopcard-list").empty();
+
+        json.forEach(function (shop) {
+            $.get("../shop-card.html", function (data) {
+
+
+                if (shop.isop !== null) data = data.replace('{shop-opened-status}', openedElementList[shop.isop ? 0 : 1]);
+                else data = data.replace('{shop-opened-status}', openedElementList[3]);
+
+                if (shop.knsta !== null) {
+                    let asktn_img = '';
+                    [...shop.knsta].forEach(c => { asktn_img = asktn_img.concat(`<img src="/resources/${c}.png" width="20" /> `); });
+                    data = data.replace('{shop-network-status}', asktn_img);
+                }
+                else data = data.replace('{shop-network-status}', '');
+
+                if (shop.name !== null) data = data.replace('{shop-name-text}', shop.name);
+                else data = data.replace('{shop-name-text}', '정보 없음');
+
+                if (shop.address !== null) data = data.replace('{shop-card-address-text}', shop.address);
+                else data = data.replace('{shop-card-address-text}', '정보 없음');
+
+                if (shop.uptime !== null) data = data.replace('{shop-card-uptime-text}', shop.uptime);
+                else data = data.replace('{shop-card-uptime-text}', '정보 없음');
+
+                shopsHtml = shopsHtml.concat(data);
+                $("#shopcard-list").append(data);
+            });
+        });
     });
 }
