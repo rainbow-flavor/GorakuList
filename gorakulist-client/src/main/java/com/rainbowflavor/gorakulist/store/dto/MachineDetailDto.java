782f1edfd32ea211e4e597555b54dcf8b7801e87
irostub <irostub@gmail.com>
2022-01-19 00:56:32 +0900

    Fix store detail page Fix store page search function - do not valid city2 value is empty Fix cs form when move cs page

--- gorakulist-client/src/main/java/com/rainbowflavor/gorakulist/store/dto/MachineDetailDto.java
+++ gorakulist-client/src/main/java/com/rainbowflavor/gorakulist/store/dto/MachineDetailDto.java
@@ -1,4 +1,15 @@
 package com.rainbowflavor.gorakulist.store.dto;
 
+import com.rainbowflavor.gorakulist.domain.Machine;
+import lombok.Data;
+
+@Data
 public class MachineDetailDto {
+    private Long machineId;
+    private String name;
+
+    public MachineDetailDto(Machine machine) {
+        this.machineId = machineId;
+        this.name = machine.getKoName();
+    }
 }
