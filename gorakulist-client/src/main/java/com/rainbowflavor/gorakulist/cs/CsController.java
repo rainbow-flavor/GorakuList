bc59d67ea2550cdcf709c0083f10ae55d97e25e0
irostub <irostub@gmail.com>
2022-01-21 06:57:06 +0900

    Remove useless importing class

--- gorakulist-client/src/main/java/com/rainbowflavor/gorakulist/cs/CsController.java
+++ gorakulist-client/src/main/java/com/rainbowflavor/gorakulist/cs/CsController.java
@@ -5,7 +5,6 @@ import com.rainbowflavor.gorakulist.webhook.DiscordWebhookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
--- gorakulist-client/src/main/java/com/rainbowflavor/gorakulist/store/StoreController.java
+++ gorakulist-client/src/main/java/com/rainbowflavor/gorakulist/store/StoreController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.constraints.NotEmpty;
 import java.util.HashSet;
