b5853d06976a539adc19e2c44bf5f4f4d387aa4b
irostub <irostub@gmail.com>
2022-01-21 21:11:12 +0900

    Fix redirect url

--- gorakulist-client/src/main/resources/static/js/content/store/store.js
+++ gorakulist-client/src/main/resources/static/js/content/store/store.js
@@ -1,6 +1,7 @@
 $(function addEventListener() {
     $("#btn-search").on("click", submitForm);
     $("#city1").on("change", onFirstAddressChange);
+    $(".store-card").on("click", onCardClickHandler)
 });
 
 function isValid(city1, selectedGame){
@@ -30,6 +31,11 @@ function submitForm(){
     $("#searchRequestForm").submit();
 }
 
+function onCardClickHandler(e){
+    e.stopPropagation();
+    e.preventDefault();
+    location.href="https://www.gorakulist.kr/store/detail?storeId="+e.currentTarget.id;
+}
 
 function onFirstAddressChange() {
     const city1 = $("#city1").val();
