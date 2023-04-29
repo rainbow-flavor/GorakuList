2d396b4a9a970da7f19ad4c400096bb1a38d2b73
irostub <irostub@gmail.com>
2023-04-29 19:26:08 +0900

    Add cors setting

--- goraku-api-server/src/main/java/com/rainbowflavor/gorakulist/config/WebConfig.java
+++ goraku-api-server/src/main/java/com/rainbowflavor/gorakulist/config/WebConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://api.goraku.ooo");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
     }
 }
