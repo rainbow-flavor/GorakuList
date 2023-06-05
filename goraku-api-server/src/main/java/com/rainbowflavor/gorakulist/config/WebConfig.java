0421532fc0ece1d390ba0dee91f35e3cf0adb7f8
irostub <irostub@gmail.com>
2023-06-05 22:34:00 +0900

    multi domain cors 대응

--- goraku-api-server/src/main/java/com/rainbowflavor/gorakulist/config/WebConfig.java
+++ goraku-api-server/src/main/java/com/rainbowflavor/gorakulist/config/WebConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://goraku.iro.ooo");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://goraku.iro.ooo", "https://gorakulist.kr", "https://www.gorakulist.kr");
     }
 }
